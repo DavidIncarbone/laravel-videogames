99e36244bf145b11acff74cf509629053b4507fe
DavidIncarbone <david.incarbone@gmail.com>
2025-07-07 12:19:27 +0200

    add autofocus on mail on login page

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,42 +1,30 @@
-@extends("layouts.master")
+@extends('layouts.master')
 
-@section("content")
+@section('content')
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-lg-8">
                 <div class="card">
                     <div class="card-header">
-                        {{ __("Per accedere a tutti i contenuti del Back Office devi essere autenticato") }}
+                        {{ __('Per accedere a tutti i contenuti del Back Office devi essere autenticato') }}
                     </div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route("login") }}">
+                        <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="mb-4 row">
-                                <label
-                                    for="email"
-                                    class="col-lg-4 col-form-label text-lg-right"
-                                >
-                                    {{ __("Indirizzo E-Mail") }}
+                                <label for="email" class="col-lg-4 col-form-label text-lg-right">
+                                    {{ __('Indirizzo E-Mail') }}
                                 </label>
 
                                 <div class="col-lg-6">
-                                    <input
-                                        id="email"
-                                        type="text"
-                                        class="form-control @error("email") is-invalid @enderror"
-                                        name="email"
-                                        value="{{ old("email") }}"
-                                        autocomplete="email"
-                                        autofocus
-                                    />
+                                    <input id="email" type="text"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" autocomplete="email" autofocus />
 
-                                    @error("email")
-                                        <span
-                                            class="invalid-feedback"
-                                            role="alert"
-                                        >
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
@@ -44,27 +32,17 @@
                             </div>
 
                             <div class="mb-4 row">
-                                <label
-                                    for="password"
-                                    class="col-lg-4 col-form-label text-lg-right"
-                                >
-                                    {{ __("Password") }}
+                                <label for="password" class="col-lg-4 col-form-label text-lg-right">
+                                    {{ __('Password') }}
                                 </label>
 
                                 <div class="col-lg-6">
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        class="form-control @error("password") is-invalid @enderror"
-                                        name="password"
-                                        autocomplete="current-password"
-                                    />
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        autocomplete="current-password" />
 
-                                    @error("password")
-                                        <span
-                                            class="invalid-feedback"
-                                            role="alert"
-                                        >
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
@@ -74,53 +52,31 @@
                             <div class="mb-4 row">
                                 <div class="col-lg-6 offset-lg-4">
                                     <div class="form-check d-flex gap-2">
-                                        <input
-                                            class="form-check-input"
-                                            type="checkbox"
-                                            name="remember"
-                                            id="remember"
-                                            {{ old("remember") ? "checked" : "" }}
-                                            style="width: 15px; height: 15px"
-                                        />
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                            {{ old('remember') ? 'checked' : '' }} style="width: 15px; height: 15px" />
 
-                                        <label
-                                            class="form-check-label"
-                                            for="remember"
-                                        >
-                                            {{ __("Ricordami") }}
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Ricordami') }}
                                         </label>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="mb-4 row mb-0">
-                                <div
-                                    class="col-lg-8 offset-lg-4 d-flex align-items-center gap-1"
-                                >
+                                <div class="col-lg-8 offset-lg-4 d-flex align-items-center gap-1">
                                     <div>
-                                        <button
-                                            type="submit"
-                                            class="btn btn-dark"
-                                        >
-                                            {{ __("Login") }}
+                                        <button type="submit" class="btn btn-dark">
+                                            {{ __('Login') }}
                                         </button>
                                     </div>
-                                    <div
-                                        class="h-100 d-flex align-items-center"
-                                    >
-                                        <a
-                                            href="{{ route("register") }}"
-                                            class="btn btn-dark"
-                                        >
+                                    <div class="h-100 d-flex align-items-center">
+                                        <a href="{{ route('register') }}" class="btn btn-dark">
                                             Registrati
                                         </a>
                                     </div>
-                                    @if (Route::has("password.request"))
-                                        <a
-                                            class="btn btn-link text-dark"
-                                            href="{{ route("password.request") }}"
-                                        >
-                                            {{ __("Password dimenticata?") }}
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link text-dark" href="{{ route('password.request') }}">
+                                            {{ __('Password dimenticata?') }}
                                         </a>
                                     @endif
                                 </div>
@@ -132,3 +88,16 @@
         </div>
     </div>
 @endsection
+
+{{-- SCRIPT --}}
+
+<script>
+    document.addEventListener('click', function(event) {
+        const emailInput = document.getElementById('email');
+
+        // Se il click NON è sull'input stesso
+        if (!emailInput.contains(event.target)) {
+            emailInput.focus();
+        }
+    });
+</script>
